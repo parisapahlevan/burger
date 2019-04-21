72b0326dc8818e942946f3348e491e072ac16a9f
Parisa Pahlevan <parisapahlevan@Zouhairs-MacBook-Pro-2.local>
2019-04-21 18:19:15 -0400

    some more changes

--- burger/db/seeds.sql
+++ burger/db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO  burgers (name) VALUES ('chicken mashroom');
+INSERT INTO  burgers (name) VALUES ('persian vegtable');
+INSERT INTO  burgers(name, devoured) VALUES ('lamb', true);
+INSERT INTO  burgers(name, devoured) VALUES ('weal vegi', true);
+INSERT INTO  burgers(name, devoured VALUES ('duck', true);
+INSERT INTO  burgers (name) VALUES ('goose');
