88c674f3ada29672fa5584cb4f55e1368abf4550
Parisa Pahlevan <parisapahlevan@Zouhairs-MacBook-Pro-2.local>
2019-04-21 23:35:19 -0400

    last version

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
